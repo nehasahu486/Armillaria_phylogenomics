6b8114f2f41f902ad19b0e62e3d4ff49a1feab95
Neha Sahu <110894234+nehasahu486@users.noreply.github.com>
2022-11-29 11:08:15 +0100

    Update and rename polytomy_resolver.R to polytomy_resolv.R

---
+++ Horizontal gene transfer/polytomy_resolv.R
@@ -0,0 +1,35 @@
+############################################################################################################################################
+#############################                                                                                  #############################
+#############################  Resolve gene trees polytomies (nodes with more than two descendent branches)    #############################
+#############################                                                                                  #############################
+############################################################################################################################################
+
+##Used bifurcatr function from PDcalc package (https://rdrr.io/github/davidnipperess/PDcalc/man/bifurcatr.html) which resolves all polytomies randomly
+library(ape)
+library(RRphylo)
+library(PDcalc)
+library(phytools)
+library(parallel)
+
+##specify the directory that has polytomic gene trees
+
+poly_tree=list.files(pattern=".newick")
+poly_tree
+names(poly_tree)=gsub("\\.fas.treefile$","",wl)
+poly_tree
+
+
+myfunc=function(treename)
+{
+  treefile=poly_tree[[treename]]
+  my_tree=read.newick(treefile)
+  outfile=paste0(treename,".tree")
+  bif_res=bifurcatr(my_tree, runs=1)
+  write.tree(bif_res,outfile)
+  return(outfile)
+}
+
+res=mclapply(names(poly_tree),FUN=myfunc)
+res
+
+##the resolved gene trees will be save in the same original directory, with the suffix ".tree"
